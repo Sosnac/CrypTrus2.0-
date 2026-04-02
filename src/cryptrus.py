2a16f632a61e1559d94f137441105c147cc3805c
David Sosnac <105160225+Sosnac@users.noreply.github.com>
2026-04-02 09:45:59 +0200

    Delete cryptrus.py

--- src/cryptrus.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-# The base64 string below decodes to my original fixed script
-payload = b'IyBDcnlwVHJ1cwojIFRoaXMgaXMgdGhlIG1haW4gZW50cnkgcG9pbnQgZm9yIHRoZSBDcnlwVHJ1cyB0b29sLgppZiBfX25hbWVfXyA9PSAnX19tYWluX18nOgogICAgcHJpbnQoJ0NyeXBUcnVzIGhhcyBzdGFydGVkIScp'
-
-exec(base64.b64decode(payload).decode('utf-8'))
-
